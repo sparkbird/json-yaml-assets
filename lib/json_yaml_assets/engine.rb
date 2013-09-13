267ca8e33c7972d2ee8129de9e77b5ab93df45b7
Julian Scheid <julians37@gmail.com>
2013-09-13 13:04:58 +0200

    Support .yml extension as well

--- lib/json_yaml_assets/engine.rb
+++ lib/json_yaml_assets/engine.rb
@@ -16,7 +16,7 @@ module JsonYamlAssets
       end
 
       def evaluate(scope, locals = { }, &block)
-        if /.json.yaml(?:$|\.)/.match(file)
+        if /.json.ya?ml(?:$|\.)/.match(file)
           @output ||= MultiJson.dump(YAML.load(data),
                                      Template.global_options.clone)
         else
@@ -30,6 +30,7 @@ module JsonYamlAssets
                 :after => 'sprockets.environment') do |app|
       next unless app.assets
       app.assets.register_engine '.yaml', Template
+      app.assets.register_engine '.yml', Template
       Template.global_options = app.config.json_yaml_assets
     end
   end
